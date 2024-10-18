efbf4836c16e808f49380470d41427c4440185c3
daredevil9215 <grgo.lovric@gmail.com>
2024-10-18 16:32:06 +0300

    Updated Navbar

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
+import myImage from '../assets/fakz_icon.jpg';
 
 function Navbar() {
 
     return (
 
         <nav className="navbar">
+            <div>
+                    <img src={myImage}></img>
+            </div>
             <ul>
                 <li>
                     <Link to="/">Home</Link>
